7bb1c9cccdf55c721ee1cd45bfe8b7fd9ad73944
Edgar Lopez-Garcia <edgarylop@gmail.com>
2019-03-01 23:46:33 -0800

    Add firebase sdk and authentication sdk

---
+++ Assets/Bootloader/Bootloader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bootloader : MonoBehaviour
+{
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0f, 0f, 100f, 100f), "Load"))
+        {
+            SceneManager.LoadScene("Login/Login");
+        }
+    }
+}
---
+++ Assets/Bootloader/FirebaseReference.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+using Firebase;
+
+public class FirebaseReference : MonoBehaviour
+{
+    public static FirebaseReference Instance = null;
+    
+    private static FirebaseApp app;
+
+    void Awake()
+    {
+        Instance = this;
+
+        FirebaseApp.CheckAndFixDependenciesAsync()
+            .ContinueWith( task => {
+                app = SetupFirebaseApp(task);
+            });
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public static FirebaseApp GetFirebaseApp()
+    {
+        return app;
+    }
+
+    private static FirebaseApp SetupFirebaseApp(Task<DependencyStatus> task)
+    {
+        DependencyStatus dependencyStatus = task.Result;
+        if (dependencyStatus == DependencyStatus.Available)
+        {
+            return FirebaseApp.DefaultInstance;
+        }
+        else
+        {
+            Debug.LogWarning("Could not resolve all Firebase dependencies: " + dependencyStatus);
+            return null;
+        }
+    }
+
+}
