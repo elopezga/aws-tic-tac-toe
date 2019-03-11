9c645fac62dcaaafe03f591fd22497fe5f171cf2
Edgar Lopez-Garcia <edgarylop@gmail.com>
2019-03-10 18:59:36 -0700

    Add firebase cloud messaging

---
+++ Assets/MainMenu/Scripts/CloudMessaging.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMessaging : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+        Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    {
+        Debug.Log("Received registration token: " + token.Token);
+    }
+
+    private void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs message)
+    {
+        Debug.Log("Received a new message from: " + message.Message.From);
+    }
+}
